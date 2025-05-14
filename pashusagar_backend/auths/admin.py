7845016de11e1f05807ba025110438ad244311b9
Pawan Gautam <127656959+Pawan-gautam011@users.noreply.github.com>
2025-05-14 11:36:20 +0545

    Remove tracked .pyc and __pycache__ files

--- pashusagar_backend/auths/admin.py
+++
@@ -1,8 +0,0 @@
-# accounts/admin.py
-from django.contrib import admin
-from .models import CustomUser
-
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'role')
-
-admin.site.register(CustomUser, CustomUserAdmin)
--- pashusagar_backend/notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import NotificationListView, MarkNotificationAsReadView,DeleteNotificationView
-
-urlpatterns = [
-    path('notifications/', NotificationListView.as_view(), name='notification-list'),
-    path('notifications/<int:pk>/read/', MarkNotificationAsReadView.as_view(), name='mark-notification-read'),
-    path('notifications/delete/<int:pk>/', DeleteNotificationView.as_view(), name='delete-notification'),
-
-]
